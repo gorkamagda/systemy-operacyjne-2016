2ee8c0259a1ac51cb48be61fc363ca2b2bcae7c7
gorkamagda <gorka.magda@gmail.com>
2016-05-29 22:58:31 +0200

    Add files via upload

---
+++ badanie.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -e wiadomosc.txt ]
+
+then
+
+a=`cat wiadomosc.txt | aspell -d pl -a | grep '^*' | wc -l`
+
+if [ $a = 1 ] 
+then
+	echo "Slowo jest poprawnym wyrazem w jezyku polskim"
+
+else
+	
+echo "Slowo nie jest poprawnym wyrazem w jezyku polskim"
+
+
+fi
+rm wiadomosc.txt
+
+fi
