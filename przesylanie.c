7ce06c0c920feda18661bef215d50ab3e9bf444d
gorkamagda <gorka.magda@gmail.com>
2016-05-29 22:50:14 +0200

    Add files via upload

---
+++ przesylanie.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <unistd.h>
+
+struct buf;
+
+int main()
+{
+	struct buf buf_1;
+	int msg_id, size;
+	msg_id = msgget(klucz, IPC_CREAT | 0666);
+	klucz_1 klucz = 45200;
+	if (msg_id == -1)
+	{
+		perror("Blad przy tworzeniu kolejki komunikatow");
+		exit(1);
+	}
+	buf_1.type = 1;
+	printf("Podaj slowo wyslane do kolejki komunikatow\n");
+	scanf("%d", buf__1.word);
+	size = sizeof buf_1.word;
+	if (msgsnd(msg_id, &buf_1, size, 0) == -1)
+	{
+		perror("Blad przy wysylaniu wiadomoœci");
+		exit(1);
+	}
+	return 0;
+}
+
+struct buf
+{
+	char word[30];
+	long type;
+};
